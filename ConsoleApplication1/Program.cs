71ffbe00398696fa3a01df3dee5c67ae2a640a3a
s28313 <s28313@pjwstk.edu.pl>
2024-03-20 13:01:40 +0100

    Initial Project

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {   
+            Console.WriteLine("Pierwszy commit");
+            
+        }
+    }
+}
